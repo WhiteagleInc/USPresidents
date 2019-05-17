d09cee6a7fcf3c0b2175f02b5d253a42dcadb3d1
Martin <jrangelespinoza@outlook.com>
2019-05-17 13:27:35 -0500

    President entity

---
+++ BackEnd/USPresidentsWebAPI/USPresidentsWebAPI/Entities/President.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace USPresidentsWebAPI.Entities
+{
+    public class President
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string BirthPlace { get; set; }
+        public DateTime DeathDay { get; set; }
+        public string DeathPlace { get; set; }
+    }
+}
